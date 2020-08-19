86e222617c95d329bc89fd58185556c23e69d22a
Melih627 <melih_caliskan58@hotmail.com>
2020-08-20 02:21:34 +0300

    medieval age project v2

---
+++ MedievalAgeArena/Assets/PlayerMovement.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterController controller;
+    Vector3 move;
+    Vector3 velocity;
+    public float speed=4f;
+    float gravity = -9.81f;
+    public float jumpHeight=2f;
+    public LayerMask groundMask;
+    public GameObject R_GroundCheck;
+    public GameObject L_GroundCheck;
+    public float GroundDistance=0.3f;
+    bool R_isGround;
+    bool L_isGround;
+    
+    private void Update()
+    {
+        CheckGround();
+        Walk();
+        Jump();
+        Run();
+        Attack();
+        Gravity();
+
+
+        
+    }
+   void Walk()
+    {
+        if (!animator.GetBool("Attack"))
+        {
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            move = transform.right * x + transform.forward * z;
+            if (move != Vector3.zero)
+            {
+                animator.SetBool("Walk", true);
+            }
+            else
+            {
+                animator.SetBool("Walk", false);
+            }
+
+            controller.Move(move * speed * Time.deltaTime);
+        }
+    }
+    void Jump()
+    {
+        if (velocity.y > -2f)
+        {
+            animator.SetBool("Walk", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && R_isGround && L_isGround)
+        {
+            
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            
+        }
+    }
+    void Gravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+    void CheckGround()
+    {
+        R_isGround = Physics.CheckSphere(R_GroundCheck.transform.position, GroundDistance, groundMask);
+        L_isGround = Physics.CheckSphere(L_GroundCheck.transform.position, GroundDistance, groundMask);
+
+        if (R_isGround && L_isGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+    }
+    void Run()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed += 2;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed -= 2;
+        }
+    }
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && R_isGround && L_isGround)
+        {
+            if (!animator.GetBool("Attack"))
+            {
+                animator.SetBool("Attack", true);
+                StartCoroutine(AttackRecoilRoutine());
+            }
+            
+        }
+        
+    }
+    IEnumerator AttackRecoilRoutine()
+    {
+        yield return new WaitForSeconds(1.3f);
+        animator.SetBool("Attack", false);
+    }
+}
---
+++ MedievalAgeArena/Assets/mouseLook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    float xRotation = 0f;
+    float yRotation = 0f;
+    public float SensitivityM = 500f;
+    public Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX=Input.GetAxis("Mouse X")*Time.deltaTime* SensitivityM;
+        float mouseY=Input.GetAxis("Mouse Y")*Time.deltaTime* SensitivityM;
+        
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation,-90f,90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        Player.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+        //Player.Rotate(Vector3.up * mouseX);
+
+
+
+
+
+
+
+    }
+}
